e8994da8eba25eb83b07e4bdeb5a5426b83c96c5
Heriberto Sanchez <heriberto_daniel@hotmail.com>
2021-12-12 16:48:43 -0600

    Added horizontal recycler view

---
+++ app/src/main/java/com/example/bitsoapp20/ui/currency/AdapterCurrency.kt
@@ -0,0 +1,52 @@
+package com.example.bitsoapp20.ui.currency
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.bitsoapp20.databinding.ListCurrenciesAdapterBinding
+import com.example.bitsoapp20.domain.Currency
+
+typealias OnCurrencyClicked = (String) -> Unit
+
+class AdapterCurrency (
+    private val onCurrencyClicked: OnCurrencyClicked
+        ): ListAdapter<Currency, AdapterCurrency.CurrencyViewHolder>(DIFF_CALLBACK)
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyViewHolder {
+        return LayoutInflater.from(parent.context)
+            .let { inflater -> ListCurrenciesAdapterBinding.inflate(inflater, parent, false) }
+            .let { binding -> CurrencyViewHolder(binding, onCurrencyClicked) }
+    }
+
+    override fun onBindViewHolder(holder: CurrencyViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class CurrencyViewHolder(
+        private val binding: ListCurrenciesAdapterBinding,
+        private val onCurrencyClicked: OnCurrencyClicked,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(currency: Currency) {
+            binding.nameCurrency.text = currency.name
+            Glide.with(binding.currencyImage)
+                .load(currency.imageUrl)
+                .into(binding.currencyImage)
+
+            binding.linearLayoutCurrency.setOnClickListener {
+                onCurrencyClicked.invoke(currency.name)
+            }
+        }
+    }
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Currency>() {
+            override fun areItemsTheSame(oldItem: Currency, newItem: Currency): Boolean =
+                oldItem.code == newItem.code
+
+            override fun areContentsTheSame(oldItem: Currency, newItem: Currency): Boolean =
+                oldItem == newItem
+        }
+    }
+}
